198a4f696403ffe2b5bfd110285bafb1437fc176
123Untitled <123untitled@pm.me>
2022-01-12 05:04:58 +0100

    fix grep color issue

--- xnorm.sh
+++ xnorm.sh
@@ -6,37 +6,52 @@ COLOR="\e[32m"
 RESET="\e[0m"
 EDITOR="nvim"
 ALT_EDITOR="vim"
+GREP="grep --color=never"
 
 function NORM {
+	# execute norminette command with arg
 	SRCH=`norminette $1`
-	FILE=`grep -i "error\!" <<< "$SRCH" \
+	# search file error pattern and get file name
+	FILE=`$GREP -i "error\!" <<< "$SRCH" \
 	| awk '{print $1}' \
 	| head -1 \
-	| grep -o '[^:]*'`
+	| $GREP -o '[^:]*'`
 }
 
 function GET_ERROR {
-	LINE=`grep -i "Error:" <<< "$SRCH" | sed -n 1p`
+	# get first error line
+	LINE=`$GREP -i "Error:" <<< "$SRCH" | sed -n 1p`
+	# get the error name
 	ERRR=`awk '{print $2}' <<< "$LINE"`
+	# get the x position error
 	XPOS=`awk '{print $6}' <<< "$LINE" \
-	| grep -o '[0-9]\+'`
+	| $GREP -o '[0-9]\+'`
+	# get the y position error
 	YPOS=`awk '{print $4}' <<< "$LINE" \
-	| grep -o '[^,]*'`
+	| $GREP -o '[^,]*'`
 }
 
-
-printf "\n%18b %b\n" \
-"X N O R M" "$COLOR><$RESET"
+# print logo
+printf "\n%18b %b\n" "X N O R M" "$COLOR><$RESET"
 while true; do
+	# norminette
 	NORM;
+	# test if there is errors
 	if [ -n "$FILE" ]; then
 		GET_ERROR;
+		# print file and error name
 		printf "%18b%b\n\n" "$FILE" " /> $COLOR$ERRR$RESET";
+		# user input read
 		read -s -n 1 INPUT;
+		# test input user
 		if [ "$INPUT" = "y" ] || [ "$INPUT" = "" ]; then
+			# check text editor exist
 			if ! command -v $EDITOR &> /dev/null;
+				# replace by an alternative
 				then EDITOR=$ALT_EDITOR; fi
-			$EDITOR $FILE "+call cursor($YPOS, $XPOS)";
+			# open file at x/y error position
+			$EDITOR $FILE "+call cursor($YPOS, $XPOS)"
 		else break; fi
+	# new line when exit
 	else printf "\n"; break; fi
 done
